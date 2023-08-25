d8e163c527a1533fc7091ba7d257fd77c5df0cc4
rockcancer <143127314+rockcancer@users.noreply.github.com>
2023-08-25 00:38:26 -0500

    Update fetch_instagram.py

--- workflows/fetch_instagram.py
+++ workflows/fetch_instagram.py
@@ -1,20 +1,31 @@
 import requests
 import json
 
-ACCESS_TOKEN = 'YOUR_INSTAGRAM_ACCESS_TOKEN'
+ACCESS_TOKEN = os.environ.get('INSTAGRAM_ACCESS_TOKEN')  # Fetch the token from environment variable
 
 def fetch_instagram_photos():
     url = f"https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token={ACCESS_TOKEN}"
     response = requests.get(url)
     data = response.json()
-    # Extract media URLs or other relevant info
     media_urls = [item['media_url'] for item in data['data'] if item['media_type'] == 'IMAGE']
     return media_urls
 
 def update_website_gallery(media_urls):
-    # Open your gallery HTML file and update it with the new media URLs.
-    # This part will be specific to how your website is structured.
-    pass
+    # Assuming you have a simple gallery page with placeholders for images
+    with open('gallery.html', 'r') as file:
+        html_content = file.read()
+
+    # Insert the images in the desired location
+    new_content = ""
+    for url in media_urls:
+        new_content += f'<img src="{url}" alt="Instagram Photo">\n'
+    
+    # Replace placeholder with the images' HTML (this is a simplistic approach, adjust as needed)
+    html_content = html_content.replace("<!-- INSTAGRAM_PHOTOS -->", new_content)
+    
+    # Write the updated content back to the gallery.html
+    with open('gallery.html', 'w') as file:
+        file.write(html_content)
 
 if __name__ == "__main__":
     media_urls = fetch_instagram_photos()
